aa5c38a70658d67bf48b2c19cc3a2e4c6d89645b
hortinstein <hortinstein@gmail.com>
2013-06-19 00:13:21 +0900

    need to go to bed...we will call this a pyrat commit :-) first time I have tasted it in months.  got the addresses added to the data sctructure, having eaddrinuse error....strack I am having trouble tracking it doewn

--- test/test.js
+++ test/test.js
@@ -1,7 +1,30 @@
 var config = require('./test_config/trove.json');
 var curator = require('../index.js');
-var mock = require('./mock.js')
 var should = require('should');
+var mock1 = '';
+var mock2 = '';
+var mock3 = '';
+describe('mock riak load balancer', function() {
+	it('should configure a mock riak node', function(done) {
+		mock1 = require('./mock.js');
+		mock1.init('mock1', 2222, function(e, r) {
+			done();
+		});
+	});
+	it('should configure a second mock riak node', function(done) {
+		mock2 = require('./mock.js');
+		mock2.init('mock2', 2223, function(e, r) {
+			done();
+		});
+	});
+	it('should configure a third mock riak node', function(done) {
+		mock3 = require('./mock.js');
+		mock3.init('mock3', 2224, function(e, r) {
+			done();
+		});
+	});
+
+});
 
 describe('curator initialize', function() {
 	it('should initial a curator', function(done) {
@@ -11,23 +34,4 @@ describe('curator initialize', function() {
 		});
 	});
 	it
-});
-
-describe('mock riak load balancer', function() {
-	it('should configure a mock riak node', function(done) {
-		mock.init('mock1', 2222, function(e, r) {
-			done();
-		});
-	});
-	it('should configure a second mock riak node', function(done) {
-		mock.init('mock2', 2223, function(e, r) {
-			done();
-		});
-	});
-	it('should configure a third mock riak node', function(done) {
-		mock.init('mock3', 2224, function(e, r) {
-			done();
-		});
-	});
-	
 });
